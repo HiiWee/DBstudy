2384e72fd94b43aa9971441101f1a2751ab1f5cf
HiiWee <wpdlzhf159@gmail.com>
2021-10-29 21:57:55 +0900

    제약조건 및 시퀀스 업데이트

--- sql_note/oracle_HOSEOK_PDB1(JOIN).sql
+++ sql_note/oracle_HOSEOK_PDB1(JOIN).sql
@@ -1,7 +1,8 @@
 select * from MEMBER;
 select * from NOTICE;
+select * from N_COMMENT;
 select * from role;
-
+commit;
 
 
 -- << INNER JOIN >>
@@ -18,6 +19,7 @@ select * from member M full outer join notice N on M.ID = N.writer_id;
 select M.ID, M.NAME, count(N.ID) 게시글수
 from MEMBER M left outer join NOTICE N on M.ID = N.WRITER_ID
 group by M.ID, M.NAME;
+
 
 
 
@@ -54,7 +56,7 @@ from member M cross join NOTICE N;
 
 -- << UNION >>
 select ID, NAME from MEMBER
-    intersect
+    union all
 select writer_id, title from notice;
 
 
@@ -62,15 +64,15 @@ select writer_id, title from notice;
 select N.ID, N.TITLE, N.writer_id, M.name, count(C.id) cnt
 from MEMBER M
 right outer join NOTICE N on M.id = N.writer_id
-left outer join "COMMENT" C on N.id = C.notice_id
+left outer join N_COMMENT C on N.id = C.notice_id
 group by N.id, N.title, N.writer_id, M.name;
 -- 위의 구문들을 하나의 뷰로 만들면 간편하게 이용가능
 
-create view NOTICEVIEW as
+create or replace view NOTICEVIEW as
 select N.ID, N.TITLE, N.writer_id, M.name, count(C.id) cnt
 from MEMBER M
 right outer join NOTICE N on M.id = N.writer_id
-left outer join "COMMENT" C on N.id = C.notice_id
+left outer join N_COMMENT C on N.id = C.notice_id
 group by N.id, N.title, N.writer_id, M.name;
 
 select * from NOTICEVIEW;
@@ -83,3 +85,28 @@ select * from USER_TABLES;
 -- 사용자 테이블의 컬럼 보기
 select * from USER_TAB_COLUMNS;
 select * from USER_TAB_COLUMNS where TABLE_NAME='MEMBER';
+
+-- << 정규식 이용한 체크제약조건 >>
+
+-- 기존 체크제약조건 드랍
+alter table member drop constraint MEMBER_PHONE_CHK1;
+
+-- 삭제된 제약조건 확인
+select * from user_constraints where TABLE_NAME = 'MEMBER';
+
+-- 제약조건 추가
+alter table MEMBER
+add constraint MEMBER_PHONE_CHK1 check(REGEXP_LIKE(PHONE, '^01[01234567]-\d{3,4}-\d{4}'));
+
+-- << SEQUENCE >>
+insert into notice(ID, TITLE, WRITER_ID)
+values(NOTICE_ID_SEQ.NEXTVAL, '시퀀스 예제', 'hoseok'); -- 시퀀스 초기값을 마지막 ID+1로 변경해야함
+
+select NOTICE_ID_SEQ.NEXTVAL from Dual; --실행할때마다 값이 1씩증가 > 시퀀스
+
+-- ID값을 넣지 않고도 ID를 자동적으로 삽입하는 방법
+-- NOTICE 테이블정보 수정 버튼 > ID열 > 만든 시퀀스 선택
+-- 하지만 초기 ID값을 설정해야 거기서부터 하나씩 증가하며 삽입함
+-- 설정 완료하면
+insert into notice(TITLE, WRITER_ID)
+values('시퀀스 예제', 'hoseok');
